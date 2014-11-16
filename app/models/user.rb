bcc7c70e05f7d59815907b634fd1ba4ebed04005
ashiina <ahmad.n.shiina@gmail.com>
2014-11-16 09:59:52 +0000

    create user model, with auth

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # format
+  before_save { self.email = email.downcase }
+
+  # validations
+  validates :name, presence: true, length: { maximum:50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+    presence: true, 
+    format: { with:VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum:6 }
+
+  # password
+  has_secure_password
+
+end

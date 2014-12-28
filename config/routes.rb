7105729e30c6ea054b754f241470eb7f3efc91f9
ashiina <ahmad.n.shiina@gmail.com>
2014-12-28 00:38:21 +0000

    implemented update, signin, signout

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  match '/test', to: 'test#index', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

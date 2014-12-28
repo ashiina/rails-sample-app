db2aa5f9006540ed4bc39538606e6c56afa3a1b2
ashiina <ahmad.n.shiina@gmail.com>
2014-12-28 02:25:32 +0000

    implement destory user & admin user, also fixed factorygirl reload bug

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -86,11 +86,12 @@ describe "User pages" do
       end
     end
 
-=begin
-# TODO fix... factorygirl sequence doesnt work.
     describe "pagination" do
 
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      before(:all) { 
+        FactoryGirl.reload
+        30.times { FactoryGirl.create(:user)} 
+      }
       after(:all)  { User.delete_all }
 
       it { should have_selector('div.pagination') }
@@ -101,7 +102,27 @@ describe "User pages" do
         end
       end
     end
-=end
+
+    describe "delete links" do
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
   end
 end
 
